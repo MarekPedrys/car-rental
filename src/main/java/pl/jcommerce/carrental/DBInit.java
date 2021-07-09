eea4d4072cc58b0bae9fc974c0d3598b6fd0e94e
Marek Pedrys JC <Marek.Pedrys@jcommerce.pl>
2021-07-09 10:22:18 +0200

    sample user and reservation data added

--- src/main/java/pl/jcommerce/carrental/DBInit.java
+++ src/main/java/pl/jcommerce/carrental/DBInit.java
@@ -8,27 +8,58 @@ import pl.jcommerce.carrental.car.entity.Body;
 import pl.jcommerce.carrental.car.entity.Car;
 import pl.jcommerce.carrental.car.entity.Color;
 import pl.jcommerce.carrental.car.entity.Fuel;
+import pl.jcommerce.carrental.reservation.ReservationRepository;
+import pl.jcommerce.carrental.reservation.entity.Reservation;
+import pl.jcommerce.carrental.user.UserRepository;
+import pl.jcommerce.carrental.user.entity.User;
+
+import java.time.LocalDate;
 
 @Component
 public class DBInit {
 
-    private CarRepository carRepository;
+    private final CarRepository carRepository;
+    private final UserRepository userRepository;
+    private final ReservationRepository reservationRepository;
 
-    public DBInit(CarRepository carRepository) {
+
+    public DBInit(CarRepository carRepository, UserRepository userRepository, ReservationRepository reservationRepository) {
         this.carRepository = carRepository;
+        this.userRepository = userRepository;
+        this.reservationRepository = reservationRepository;
     }
 
     @EventListener(ApplicationReadyEvent.class)
     public void runExample() {
-        carRepository.save(new Car("Fiat", "Punto", 2014, Body.HATCHBACK, Fuel.PETROL,
-                Color.RED, 5, 3, false, true));
-        carRepository.save(new Car("Fiat", "Punto", 2017, Body.HATCHBACK, Fuel.PETROL,
-                Color.SILVER, 5, 5, false, true));
-        carRepository.save(new Car("Ford", "Pocus", 2017, Body.KOMBI, Fuel.DIESEL,
-                Color.SILVER, 5, 5, true, true));
-        carRepository.save(new Car("Fiat", "Stilo", 2019, Body.SEDAN, Fuel.DIESEL,
-                Color.SILVER, 5, 5, false, true));
-        carRepository.save(new Car("Fiat", "Stilo", 2020, Body.KOMBI, Fuel.PETROL,
-                Color.WHITE, 5, 5, false, true));
+
+        Car car1 = new Car("Fiat", "Punto", 2014, Body.HATCHBACK, Fuel.PETROL,
+                Color.RED, 5, 3, false, true);
+        Car car2 = new Car("Fiat", "Punto", 2017, Body.HATCHBACK, Fuel.PETROL,
+                Color.SILVER, 5, 5, false, true);
+        Car car3 = new Car("Fiat", "Stilo", 2019, Body.SEDAN, Fuel.DIESEL,
+                Color.SILVER, 5, 5, false, true);
+        Car car4 = new Car("Fiat", "Stilo", 2020, Body.KOMBI, Fuel.PETROL,
+                Color.WHITE, 5, 5, false, true);
+        Car car5 = new Car("Ford", "Focus", 2017, Body.KOMBI, Fuel.DIESEL,
+                Color.SILVER, 5, 5, true, true);
+        carRepository.save(car1);
+        carRepository.save(car2);
+        carRepository.save(car3);
+        carRepository.save(car4);
+        carRepository.save(car5);
+
+        User user1 = new User("Adam Adamowski", "adam@gmail.com");
+        User user2 = new User("Basia Basiowska", "basia@gmail.com");
+        User user3 = new User("Celina Celinowska", "celina@gmail.com");
+        userRepository.save(user1);
+        userRepository.save(user2);
+        userRepository.save(user3);
+
+        Reservation reservation1 = new Reservation(car1, user3, LocalDate.of(2021, 7, 13), LocalDate.of(2021, 7, 14), false);
+        Reservation reservation2 = new Reservation(car4, user2, LocalDate.of(2021, 7, 22), LocalDate.of(2021, 7, 26), true);
+        reservationRepository.save(reservation1);
+        reservationRepository.save(reservation2);
+
+
     }
 }
